f854584f54b83d1730446cbbfe4f27467190f9a8
Tamsir Diouck <tamsirdiouck@gmail.com>
2026-01-30 17:04:17 +0000

    update vercel

--- api/send-notification.ts
+++ api/send-notification.ts
@@ -38,10 +38,9 @@ try {
    🔐 SUPABASE
 ===================================================== */
 const supabase = createClient(
- "https://netgmadtongdspojqaue.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ldGdtYWR0b25nZHNwb2pxYXVlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgxMTg3NDIsImV4cCI6MjA2MzY5NDc0Mn0.h6lHxp0xUjiB2mE6OT-ePqNanmSFKs7zhvvHRtwKXKI"
+  process.env.SUPABASE_URL!,
+  process.env.SUPABASE_SERVICE_ROLE_KEY!
 );
-
 
 /* =====================================================
    🚀 HANDLER
